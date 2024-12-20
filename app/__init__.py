20c17fae7e0fe9ed362eef47d7a0075b9c183a3c
traubt <traub.tomer@gmail.com>
2024-12-20 12:21:31 +0200

    Implementing toc_sales_log notification

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,13 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tasteofc_wp268:]44p7214)S@176.58.117.107/tasteofc_wp268'
     db.init_app(app)
 
+    # SMTP server configuration
+    SMTP_SERVER = 'mail.tasteofcannabis.co.za'
+    SMTP_PORT = 465
+    SENDER_EMAIL = 'dashboard@tasteofcannabis.co.za'
+    SENDER_PASSWORD = 'Plmokn098!@'
+    RECIPIENT_EMAIL = 'traub.tomer@outlook.com'
+
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
