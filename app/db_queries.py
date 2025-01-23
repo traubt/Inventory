9a8e7096ca257c95ee2b8f90ddbc35fdeea0fc59
traubt <traub.tomer@gmail.com>
2025-01-23 09:10:08 +0200

    Fix update password functionality

--- app/db_queries.py
+++ app/db_queries.py
@@ -744,7 +744,7 @@ WHERE
     st.shop_name = %s -- Filter to include only entries for the specified shop
 ORDER BY 
     COALESCE(s.sales_since_stock_read, 0) DESC;
-            '''
+    '''
 
     # Execute the query with the parameter
     cursor.execute(query, (shop,shop))
